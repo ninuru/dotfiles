a8a5568e980b6a1d2106fffdcd950f14d24b2f8e
Dominik <dominik.schreider@yahoo.com>
2026-07-16 10:29:19 +0200

    [nvim, waybar] quickfix nav on <M-n>/<M-p>, waybar only on primary monitor
    - nvim: move quickfix next/prev from <M-j>/<M-k> to <M-n>/<M-p> with zz
      centering, disable auto-pairs shortcuts that shadowed the maps
    - waybar: show the bar only on eDP-1 (laptop monitor)

--- .config/nvim/lua/plugins/quickfix.lua
+++ .config/nvim/lua/plugins/quickfix.lua
@@ -1,3 +1,7 @@
-vim.keymap.set("n", "<M-j>", "<Cmd>try | cnext | catch | cfirst | catch | endtry<CR>", { silent = true })
-vim.keymap.set("n", "<M-k>", "<Cmd>try | cprevious | catch | clast | catch | endtry<CR>", { silent = true })
+-- auto-pairs maps <M-n>/<M-p> buffer-locally, shadowing the quickfix maps below
+vim.g.AutoPairsShortcutJump = ""
+vim.g.AutoPairsShortcutToggle = ""
+
+vim.keymap.set("n", "<M-n>", "<Cmd>try | cnext | catch | cfirst | endtry<CR>zz")
+vim.keymap.set("n", "<M-p>", "<Cmd>try | cprevious | catch | clast | endtry<CR>zz")
 return {};
